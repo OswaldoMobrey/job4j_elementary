239d3d88abcf36c048cb664c7421e5d4693a797c
OswaldoMobrey <joker_qwerty@mail.ru>
2023-11-01 12:14:16 +0500

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -30,9 +30,9 @@ class X2Test {
 
     @Test
     void whenA1B1C0X1Then2() {
-        int a = 0;
+        int a = 1;
         int b = 1;
-        int c = 1;
+        int c = 0;
         int x = 1;
         int expected = 2;
         int rsl = X2.calc(a, b, c, x);
@@ -40,12 +40,12 @@ class X2Test {
     }
 
     @Test
-    void whenA1B1C1X0Then2() {
-        int a = 0;
+    void whenA1B1C1X0Then1() {
+        int a = 1;
         int b = 1;
         int c = 1;
-        int x = 1;
-        int expected = 2;
+        int x = 0;
+        int expected = 1;
         int rsl = X2.calc(a, b, c, x);
         assertThat(rsl).isEqualTo(expected);
     }
