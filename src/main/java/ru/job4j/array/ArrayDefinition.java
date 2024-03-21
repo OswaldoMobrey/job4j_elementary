0f569cedb75eda1ad4b4df73fb09c0786014a2d1
OswaldoMobrey <joker_qwerty@mail.ru>
2024-03-21 22:56:46 +0500

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Ignat Laptev";
+        names[1] = "Veniamin Sluhin";
+        names[2] = "Eduard Vareniy";
+        names[3] = "Igor Letov";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
